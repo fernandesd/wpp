c30af7bbf42693c5c22a21029edea467c2558d87
itamar <itamarjoire@infojr.com.br>
2018-06-12 12:54:32 -0300

    Inicia frontend

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Link Para conversa WhatsApp</title>
-    </head>
+<head>
+<meta charset="utf-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Meu link WhatsApp</title>
+<link rel="stylesheet" href="assets/css/gaintime.min.css" media ="screen" title="no title">
+<link rel="stylesheet" href="assets/css/style.css" media ="screen" title="no title">
+</head>
     <body>
-        <form class="" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+      <div class="formulario content-align center">
+        <form class="gt-form center table-cell full" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            <div class="input">
+                <input class="padding" type="tel" name="fone" placeholder=" Código do país + DDD + Número do WhatsApp. Ex: 55 71 98888-8888" autofocus required size="55%">
+            </div>
 
-            <input type="tel" name="fone" placeholder=" Código do país + DDD + Número do WhatsApp. Ex: 55 71 98888-8888" autofocus required size="55%"><br><br>
+            <div class="input">
+                <textarea class="padding" name="msg" rows="3" cols="54" placeholder="#opcional - Insira uma mensagem legal..." maxlength="144" wrap=""></textarea>
+            </div>
 
-            <textarea name="msg" rows="3" cols="54" placeholder="#opcional - Insira uma mensagem legal..." maxlength="144" wrap=""></textarea><br><br>
-
-            <button type="submit" name="enviar">Obter Link</button>
+            <button class="btn danger lg right" type="submit" name="enviar" data-modal="Modal">Obter Link</button>
 
         </form>
-    </body>
-</html>
+        </div>
+
 
 <?php
 
@@ -46,10 +55,22 @@ if(isset($_POST['enviar']))
 
     $number = filter_var($separado, FILTER_SANITIZE_NUMBER_INT);
 
-    echo "https://api.whatsapp.com/send?phone=$number&text=$text";
+    //echo "https://api.whatsapp.com/send?phone=$number&text=$text";
 }
-
-
-
-
  ?>
+
+<div class="gt-modal">
+<!-- Aqui você vai atribuir um id ao seu modal -->
+  <div id="Modal" class="modal">
+<!-- Este botão é responsável por fechar o modal -->
+  <button class="modal-close" type="button">×</button>
+    <div class="modal-body">
+<!-- Digite o conteúdo do modal aqui -->
+  <?php echo "https://api.whatsapp.com/send?phone=$number&text=$text"; ?>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="assets/js/gaintime.min.js"></script>
+
+</body>
+</html>
